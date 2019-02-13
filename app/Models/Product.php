f991a4de71f6360e16b152c2b22e8c813a2393d2
Emeka Augustine <cafafanscoders@gamil.com>
2019-02-12 20:37:48 -0500

    Created a Relationship between Product and Review

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
 
 class Product extends Model
 {
-    //
+    /**
+     * Relationship between Product and Review
+     * One to Many Relationship: A product can have many reviews
+     * @return object
+     */
+
+     public function reviews() {
+         return $this->hasMany(Review::class);
+     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Review extends Model
 {
-    //
+    /**
+     * Relationship between Review and Product
+     * Many to One Relationship: Many reviews can belong to one produt
+     * @return object
+     */
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
